2fd759233ec7fd8ffbc82cff7a48075c18e46f8b
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-04 17:58:26 -0600

    Add script to download and install VSCode on Linux

---
+++ VSCode.sh
@@ -0,0 +1,3 @@
+wget -O VSCode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+sudo apt install --no-install-recommends --no-install-suggests ./VSCode.deb -y
+rm VSCode.deb
