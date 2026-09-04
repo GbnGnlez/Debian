f5946c235682f0475dd2a849710e3e072564b2fa
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-04 17:29:36 -0600

    Add script to install desktop environment packages
    Install essential desktop environment packages.

---
+++ DesktopEnvironment.sh
@@ -0,0 +1,14 @@
+sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && \
+sudo apt install --no-install-recommends --no-install-suggests -y \
+  bibata-cursor-theme \
+  dolphin \
+  firefox-esr \
+  kde-spectacle \
+  kdeconnect \
+  konsole \
+  papirus-icon-theme \
+  plasma-desktop \
+  plasma-nm \
+  plasma-pa \
+  powerdevil \
+  sddm
