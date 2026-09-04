fbe7cf91581a6c8e2b7e17b67e045a0013ca1d06
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-04 17:56:33 -0600

    Add installation script for OnlyOffice
    Download and install OnlyOffice desktop editors.

---
+++ OnlyOffice.sh
@@ -0,0 +1,3 @@
+wget -O OnlyOffice.deb "https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/onlyoffice-desktopeditors_amd64.deb"
+sudo apt install --no-install-recommends --no-install-suggests ./OnlyOffice.deb -y
+rm OnlyOffice.deb
