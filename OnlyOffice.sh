a2ade062c38b72ce761f43a75ca6e52522f1fea6
GbnGnlez <GbnGnlez@outlook.com>
2026-09-05 06:33:01 -0600

    Fix wget command in OnlyOffice.sh to suppress output for cleaner execution

--- OnlyOffice.sh
+++ OnlyOffice.sh
@@ -4,9 +4,9 @@ INSTALLED_VER=$(onlyoffice-desktopeditors --version 2>&1 | grep -oP '\d+\.\d+\.\
 LATEST_VER=$(curl -sI "https://github.com/ONLYOFFICE/DesktopEditors/releases/latest" | grep -oP '/v\K\d+\.\d+\.\d+')
 
 if [ "$INSTALLED_VER" != "$LATEST_VER" ]; then
-    wget -O OnlyOffice.deb "https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/onlyoffice-desktopeditors_amd64.deb"
+    wget -qO OnlyOffice.deb "https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/onlyoffice-desktopeditors_amd64.deb"
     sudo apt install --no-install-recommends --no-install-suggests ./OnlyOffice.deb ttf-mscorefonts-installer -y
-    rm -f OnlyOffice.deb
+    rm OnlyOffice.deb
 else
     echo "OnlyOffice is already the newest version ($INSTALLED_VER)."
 fi
