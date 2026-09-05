7f5f70661b3903ff7d82aa7cba64e9525d3cecb6
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-05 04:24:05 -0600

    Add download link for ONLYOFFICE Desktop Editors

--- OnlyOffice.sh
+++ OnlyOffice.sh
@@ -1,3 +1,5 @@
+# https://github.com/ONLYOFFICE/DesktopEditors/releases/
+
 wget -O OnlyOffice.deb "https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/onlyoffice-desktopeditors_amd64.deb"
 sudo apt install --no-install-recommends --no-install-suggests ./OnlyOffice.deb -y
 rm OnlyOffice.deb
