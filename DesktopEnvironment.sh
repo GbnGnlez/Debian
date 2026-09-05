efee0a8cb5a81c94ecdebc1601e09faf91dc66d3
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-05 01:50:37 -0600

    Clean up DesktopEnvironment.sh by removing blank lines
    Removed unnecessary blank lines in package installation list.

--- DesktopEnvironment.sh
+++ DesktopEnvironment.sh
@@ -3,15 +3,12 @@ sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 sudo apt install --no-install-recommends --no-install-suggests -y \
   git \
   firefox-esr \
-  
   dolphin \
   kde-spectacle \
   plasma-desktop \
   plasma-nm \
   plasma-pa \
   powerdevil \
-  
   sddm-theme-breeze \
   sddm \
-
   wireplumber
