6eec6cffb772c4b4be74314679bb6d1941646c29
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-05 02:44:12 -0600

    Install Microsoft core fonts alongside OnlyOffice
    Added installation of Microsoft core fonts.

--- OnlyOffice.sh
+++ OnlyOffice.sh
@@ -1,3 +1,5 @@
 wget -O OnlyOffice.deb "https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/onlyoffice-desktopeditors_amd64.deb"
 sudo apt install --no-install-recommends --no-install-suggests ./OnlyOffice.deb -y
 rm OnlyOffice.deb
+
+sudo apt install --no-install-recommends --no-install-suggests ttf-mscorefonts-installer -y
