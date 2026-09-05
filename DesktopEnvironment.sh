f94988049d2dfd934ddedafb9c60cae361c15aef
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-05 01:03:35 -0600

    Add git to the list of installed packages

--- DesktopEnvironment.sh
+++ DesktopEnvironment.sh
@@ -1,6 +1,7 @@
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 
 sudo apt install --no-install-recommends --no-install-suggests -y \
+  git \
   dolphin \
   firefox-esr \
   kde-spectacle \
