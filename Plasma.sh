83dca5bd8cb4ad4f413719911a31a3e9c532adb5
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-05 01:13:42 -0600

    Remove duplicate icon theme configuration command
    Removed duplicate kwriteconfig6 command for setting the icon theme.

--- Plasma.sh
+++ Plasma.sh
@@ -23,8 +23,8 @@ plasma-apply-desktoptheme "$DesktopTheme"
 plasma-apply-colorscheme "$ColorScheme"
 plasma-apply-cursortheme "$CursorTheme"
 
+kwriteconfig6 --file kdeglobals --group Icons --key Theme Papirus-"$1"
 wget -qO- https://git.io/papirus-folders-install | sh
 papirus-folders --color "$2"
-kwriteconfig6 --file kdeglobals --group Icons --key Theme Papirus-"$1"
 
 kwriteconfig6 --file ksplashrc --group KSplash --key Theme None
