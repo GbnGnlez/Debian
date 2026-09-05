9f192677a7c68410183153223364001faa2e5921
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-04 20:07:23 -0600

    Update color options for IdeaPad and IdeaCentre

--- Build.sh
+++ Build.sh
@@ -4,6 +4,6 @@
 case "$1" in
   ThinkPad)   ./Plasma.sh Dark blue; ./VSCode.sh; ./Spotify.sh ;;
   Pavilion)   ./Plasma.sh Light pink; ./Spotify.sh ;;
-  IdeaPad)    ./Plasma.sh Dark green;;
-  IdeaCentre) ./Plasma.sh Light black;;
+  IdeaPad)    ./Plasma.sh Dark pink;;
+  IdeaCentre) ./Plasma.sh Light pink;;
 esac
