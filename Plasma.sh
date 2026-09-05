7426591c390ecc8325b8cf7f1e42103afc89bd10
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-04 18:03:22 -0600

    Add Plasma.sh for KDE Plasma theme setup

---
+++ Plasma.sh
@@ -0,0 +1,6 @@
+sudo apt install --no-install-recommends --no-install-suggests bibata-cursor-theme -y && \
+
+plasma-apply-lookandfeel --apply org.kde.breezedark.desktop && \
+plasma-apply-desktoptheme breeze-dark && \
+plasma-apply-colorscheme BreezeDark && \
+plasma-apply-cursortheme Bibata-Modern-Ice
