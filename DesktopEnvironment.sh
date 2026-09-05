853b0eae019e796f232bae0dbf6a90de4e00278c
Gibran González <146513259+GbnGnlez@users.noreply.github.com>
2026-09-04 18:04:20 -0600

    Remove bibata-cursor-theme from installation script
    Removed 'bibata-cursor-theme' from installation list.

--- DesktopEnvironment.sh
+++ DesktopEnvironment.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && \
+
 sudo apt install --no-install-recommends --no-install-suggests -y \
-  bibata-cursor-theme \
   dolphin \
   firefox-esr \
   kde-spectacle \
