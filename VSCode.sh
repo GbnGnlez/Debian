d92ba49adf6d2efde503782d8e67ae7265bf6146
GbnGnlez <GbnGnlez@outlook.com>
2026-09-05 05:36:27 -0600

    Uncomment installation commands in VSCode.sh for clarity and functionality

--- VSCode.sh
+++ VSCode.sh
@@ -1,8 +1,8 @@
 # https://code.visualstudio.com/Download/
 
-# wget -O VSCode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-# sudo apt install --no-install-recommends --no-install-suggests ./VSCode.deb shfmt -y
-# rm VSCode.deb
+wget -O VSCode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+sudo apt install --no-install-recommends --no-install-suggests ./VSCode.deb shfmt -y
+rm VSCode.deb
 
 git config --global user.name "GbnGnlez"
 git config --global user.email "GbnGnlez@outlook.com"
