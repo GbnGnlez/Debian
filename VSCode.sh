176947973922385f23ce28d92bf89e0c6651f639
GbnGnlez <GbnGnlez@outlook.com>
2026-09-05 04:44:01 -0600

    Fix installation command and update VSCode settings for formatting and smart commit

--- VSCode.sh
+++ VSCode.sh
@@ -1,7 +1,7 @@
 # https://code.visualstudio.com/Download/
 
 wget -O VSCode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-sudo apt install --no-install-recommends --no-install-suggests ./VSCode.deb -y
+sudo apt install --no-install-recommends --no-install-suggests ./VSCode.deb shfmt -y
 rm VSCode.deb
 
 git config --global user.name "GbnGnlez"
@@ -10,8 +10,8 @@ git config --global user.email "GbnGnlez@outlook.com"
 mkdir -p ~/.config/Code/User
 cat > ~/.config/Code/User/settings.json <<'EOF'
 {
-    "git.enableSmartCommit": true,
-        "editor.formatOnSave": true
+            "editor.formatOnSave": true,
+            "git.enableSmartCommit": true
 }
 EOF
  
