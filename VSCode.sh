39ca9a75b7d8aac103908fe01d6b39188b52bd41
GbnGnlez <GbnGnlez@outlook.com>
2026-09-05 05:35:50 -0600

    Comment out installation commands in VSCode.sh for clarity and maintain formatting consistency

--- VSCode.sh
+++ VSCode.sh
@@ -1,8 +1,8 @@
 # https://code.visualstudio.com/Download/
 
-wget -O VSCode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-sudo apt install --no-install-recommends --no-install-suggests ./VSCode.deb -y
-rm VSCode.deb
+# wget -O VSCode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+# sudo apt install --no-install-recommends --no-install-suggests ./VSCode.deb shfmt -y
+# rm VSCode.deb
 
 git config --global user.name "GbnGnlez"
 git config --global user.email "GbnGnlez@outlook.com"
@@ -14,3 +14,5 @@ cat > ~/.config/Code/User/settings.json << 'EOF'
     "git.enableSmartCommit": true 
 }
 EOF
+
+shfmt -w -s -bn -ci -sr -fn .
